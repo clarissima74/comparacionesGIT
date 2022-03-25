0283843a72bc6e2f86b038c536680118a742f4e7
'clara <clarissima74@gmail.com>
2022-03-25 12:36:57 +0100

    Subimos mensajes del desarrollador 1

--- src/Clase1.java
+++ src/Clase1.java
@@ -9,10 +9,9 @@ public class Clase1 {
 		// TODO Auto-generated method stub
 		System.out.println("Vamos a combinar ramas o versiones");
 
-		//Versión 1
-		//Versión 2
-		//Versión 3 arriba
-		//Versión 4 arriba
+	System.out.println("Desarrollador 1: Primer comentario");
+	System.out.println("Desarrollador 1: Segundo comentario ");
+	
 		
 
 	}
