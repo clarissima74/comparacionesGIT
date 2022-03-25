2f357b38f4b8a41b62a9e162047b47010c74e71b
'clara <clara.catala@iessampedro.org>
2022-03-25 12:11:36 +0100

    Primera Subida

---
+++ src/Clase1.java
@@ -0,0 +1,15 @@
+
+public class Clase1 {
+
+	public Clase1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Vamos a combinar ramas o versiones");
+		//Versión 1
+
+	}
+
+}
